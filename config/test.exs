106d96b5f433221db859bec63dde4bbd37a28987
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-05-10 10:31:26 -0500

    Generate Umbrella without html and assets

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :mintacoin, Mintacoin.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "mintacoin_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :mintacoin_web, MintacoinWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "m7eyXj0+Vv6yki1j/e+A6hKcL5LhmPBtnPsFblmNo1TL+RmBH3I/w5AbC4thMpai",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# In test we don't send emails.
+config :mintacoin, Mintacoin.Mailer, adapter: Swoosh.Adapters.Test
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
