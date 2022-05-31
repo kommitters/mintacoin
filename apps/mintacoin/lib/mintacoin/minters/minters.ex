bca8106e51cf91b0d48c6c488fe292b7aa974c48
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-05-31 09:44:45 -0500

    Fix typespecs of Minters context

--- apps/mintacoin/lib/mintacoin/minters/minters.ex
+++ apps/mintacoin/lib/mintacoin/minters/minters.ex
@@ -9,33 +9,34 @@ defmodule Mintacoin.Minters do
   @type id :: String.t()
   @type api_key :: String.t()
   @type changes :: map()
-  @type minter :: term()
-  @type error :: term()
+  @type minter :: any()
+  @type error :: any()
 
   @archived_status :archived
 
-  @spec create_minter(changes) :: {:ok, Minter.t()} | {:error, error}
+  @spec create_minter(changes :: changes()) :: {:ok, Minter.t()} | {:error, error()}
   def create_minter(changes) do
     %Minter{}
     |> Minter.create_changeset(changes)
     |> Repo.insert()
   end
 
-  @spec archive_minter(id) :: {:ok, Minter.t()} | {:error, error}
+  @spec archive_minter(id :: id()) :: {:ok, Minter.t()} | {:error, error()}
   def archive_minter(id) do
     Minter
     |> Repo.get(id)
     |> persist_changes(%{status: @archived_status})
   end
 
-  @spec is_authorized(api_key) :: boolean()
-  def is_authorized(api_key) do
+  @spec is_authorized?(api_key :: api_key()) :: boolean()
+  def is_authorized?(api_key) do
     Minter
     |> Repo.get_by(api_key: api_key)
-    |> has_minter_access()
+    |> has_minter_access?()
   end
 
-  @spec persist_changes(minter, changes) :: {:ok, Minter.t()} | {:error, error}
+  @spec persist_changes(minter :: minter(), changes :: changes()) ::
+          {:ok, Minter.t()} | {:error, error()}
   defp persist_changes(nil, _changes), do: {:error, :not_found}
 
   defp persist_changes(minter, changes) do
@@ -44,7 +45,7 @@ defmodule Mintacoin.Minters do
     |> Repo.update()
   end
 
-  @spec has_minter_access(minter) :: boolean()
-  defp has_minter_access(nil), do: false
-  defp has_minter_access(%{status: status}), do: status != @archived_status
+  @spec has_minter_access?(minter :: minter()) :: boolean()
+  defp has_minter_access?(nil), do: false
+  defp has_minter_access?(%{status: status}), do: status != @archived_status
 end
