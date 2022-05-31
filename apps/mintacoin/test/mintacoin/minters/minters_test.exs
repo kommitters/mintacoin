7707ffacc7826ae6fc3ceb4ffe2dc9ff19e0ea22
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-05-31 15:14:31 -0500

    Improve Minters test

--- apps/mintacoin/test/mintacoin/minters/minters_test.exs
+++ apps/mintacoin/test/mintacoin/minters/minters_test.exs
@@ -3,42 +3,44 @@ defmodule Database.Accounts.AccountsTest do
     This module is used to group common tests for Minters functions
   """
 
-  use ExUnit.Case, async: false
+  use Mintacoin.DataCase, async: false
 
-  alias Mintacoin.Minters
+  alias Mintacoin.{Minter, Minters}
   alias Ecto.Adapters.SQL.Sandbox
 
   setup do
     :ok = Sandbox.checkout(Mintacoin.Repo)
   end
 
-  describe "minters creation" do
+  describe "create_minter/1" do
     setup do
       %{
-        full_minter: %{
+        minter: %{
           email: "test@mail.com",
           name: "Test Minter",
           api_key: "LTS3c860j40uU2MaAmnHA4kQKV5mvHDhSKwCgP3vFpU"
-        },
-        incomplete_minter: %{
-          name: "Test Minter 2",
-          api_key: "vPKbiO11yQ5JKnRn0Rpfo+WiJy9KxM8SoJbpFq2ybbk"
         }
       }
     end
 
-    test "Create minter", %{full_minter: %{email: email} = minter_data} do
-      {:ok, %{email: ^email}} = Minters.create_minter(minter_data)
+    test "with valid params", %{minter: %{email: email} = minter_data} do
+      {:ok, %Minter{email: ^email}} = Minters.create_minter(minter_data)
     end
 
-    test "validate required fields", %{incomplete_minter: minter_data} do
-      {:error, _error} = Minters.create_minter(minter_data)
+    test "validate required fields" do
+      {:error, changeset} = Minters.create_minter(%{})
+
+      %{
+        api_key: ["can't be blank"],
+        email: ["can't be blank"],
+        name: ["can't be blank"]
+      } = errors_on(changeset)
     end
   end
 
-  describe "minters archive" do
+  describe "archive_minter/1" do
     setup do
-      {:ok, %{id: id}} =
+      {:ok, %Minter{id: id}} =
         Minters.create_minter(%{
           email: "test3@mail.com",
           name: "Test Minter 3",
@@ -48,31 +50,32 @@ defmodule Database.Accounts.AccountsTest do
       %{id: id}
     end
 
-    test "archive minter with given id", %{id: id} do
-      {:ok, %{status: :archived}} = Minters.archive_minter(id)
+    test "with valid id, archive minter", %{id: id} do
+      {:ok, %Minter{status: :archived}} = Minters.archive_minter(id)
     end
 
-    test "not found minter" do
-      {:error, _error} = Minters.archive_minter("32c2a7a2-ebb9-4b28-a199-cd34234b2d58")
+    test "with not valid id, return not found error" do
+      {:error, :not_found} = Minters.archive_minter("32c2a7a2-ebb9-4b28-a199-cd34234b2d58")
     end
   end
 
-  describe "minters validate access" do
+  describe "is_authorized?/1" do
     setup do
-      Minters.create_minter(%{
-        email: "test4@mail.com",
-        name: "Test Minter 4",
-        api_key: "J6CwjuzoR3I2hGzXFFUz7mtp/insEoadWQ80nDUOkCw"
-      })
+      {:ok, %Minter{api_key: api_key}} =
+        Minters.create_minter(%{
+          email: "test4@mail.com",
+          name: "Test Minter 4",
+          api_key: "J6CwjuzoR3I2hGzXFFUz7mtp/insEoadWQ80nDUOkCw"
+        })
 
-      %{api_key: "J6CwjuzoR3I2hGzXFFUz7mtp/insEoadWQ80nDUOkCw"}
+      %{api_key: api_key}
     end
 
-    test "with an authorized key", %{api_key: api_key} do
+    test "return true with an authorized key", %{api_key: api_key} do
       true = Minters.is_authorized?(api_key)
     end
 
-    test "with an invalid key" do
+    test "rerturn false with an invalid key" do
       false = Minters.is_authorized?("INVALID")
     end
   end
