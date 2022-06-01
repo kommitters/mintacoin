f0f3418b7632b15a19c6f5a4c95bf344499de101
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-01 11:22:06 -0500

    Add typespec for minter_factory

--- apps/mintacoin/test/support/minters/minters_factory.ex
+++ apps/mintacoin/test/support/minters/minters_factory.ex
@@ -7,6 +7,7 @@ defmodule Mintacoin.MinterFactory do
 
   defmacro __using__(_opts) do
     quote do
+      @spec minter_factory(attrs :: map()) :: Minter.t()
       def minter_factory(attrs) do
         email = Map.get(attrs, :email, sequence(:email, &"minter#{&1}@example.com"))
         name = Map.get(attrs, :name, sequence(:name, &"Minter #{&1}"))
