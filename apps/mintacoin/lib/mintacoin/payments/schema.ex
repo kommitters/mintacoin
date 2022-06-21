4c34614ef9b28f6dce2496422932ad9096818f18
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-21 18:36:50 -0500

    Improve Payment schema

--- apps/mintacoin/lib/mintacoin/payments/schema.ex
+++ apps/mintacoin/lib/mintacoin/payments/schema.ex
@@ -9,14 +9,13 @@ defmodule Mintacoin.Payment do
   import EctoEnum
 
   alias Ecto.Changeset
-  alias Mintacoin.{Account, Blockchain, Asset}
+  alias Mintacoin.{Account, Asset}
 
   @type status :: :emitted | :processed
 
   @type t :: %__MODULE__{
-          source: Account.t(),
-          destination: Account.t(),
-          blockchain: Blockchain.t(),
+          source_account: Account.t(),
+          destination_account: Account.t(),
           asset: Asset.t(),
           amount: String.t(),
           status: status()
@@ -26,10 +25,20 @@ defmodule Mintacoin.Payment do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "payments" do
-    belongs_to(:source, Account, type: :binary_id)
-    belongs_to(:destination, Account, type: :binary_id)
-    belongs_to(:blockchain, Blockchain, type: :binary_id)
-    belongs_to(:asset, Asset, type: :binary_id)
+    belongs_to(:source_account, Account,
+      foreign_key: :source,
+      references: :address,
+      type: :binary_id
+    )
+
+    belongs_to(:destination_account, Account,
+      foreign_key: :destination,
+      references: :address,
+      type: :binary_id
+    )
+
+    belongs_to(:asset, Asset, foreign_key: :asset_code, references: :code, type: :string)
+
     field(:amount, :string)
     field(:status, Status, default: :emitted)
 
@@ -39,17 +48,16 @@ defmodule Mintacoin.Payment do
   @spec changeset(asset :: %__MODULE__{}, changes :: map()) :: Changeset.t()
   def changeset(asset, changes) do
     asset
-    |> cast(changes, [:source_id, :destination_id, :blockchain_id, :asset_id, :amount, :status])
+    |> cast(changes, [:source, :destination, :asset_code, :amount, :status])
     |> validate_required([
-      :source_id,
-      :destination_id,
-      :blockchain_id,
-      :asset_id,
+      :source,
+      :destination,
+      :asset_code,
       :amount
     ])
-    |> foreign_key_constraint(:source_id)
-    |> foreign_key_constraint(:destination_id)
-    |> foreign_key_constraint(:blockchain_id)
-    |> foreign_key_constraint(:asset_id)
+    |> foreign_key_constraint(:source)
+    |> foreign_key_constraint(:destination)
+    |> foreign_key_constraint(:asset_code)
+    |> validate_format(:amount, ~r/^\d+(\.\d+)?$/, message: "must have a decimal number format")
   end
 end
