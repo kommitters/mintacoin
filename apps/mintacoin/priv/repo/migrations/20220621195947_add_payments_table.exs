8679fc3a230b94b136a0e19d04e2aadf36eb9106
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-21 18:38:16 -0500

    Format migration

--- apps/mintacoin/priv/repo/migrations/20220621195947_add_payments_table.exs
+++ apps/mintacoin/priv/repo/migrations/20220621195947_add_payments_table.exs
@@ -6,7 +6,7 @@ defmodule Mintacoin.Repo.Migrations.AddPaymentsTable do
       add(:id, :uuid, primary_key: true)
       add(:source, references(:accounts, column: :address, type: :uuid), null: false)
       add(:destination, references(:accounts, column: :address, type: :uuid), null: false)
-      add(:asset_code, references(:assets, column: :code,  type: :string), null: false)
+      add(:asset_code, references(:assets, column: :code, type: :string), null: false)
       add(:amount, :string, null: false)
       add(:status, :string, null: false)
 
