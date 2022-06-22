bd5b4605cc1aac5e734b5645537fb512fdff8aa3
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-22 11:54:51 -0500

    Add Payments boundary

---
+++ apps/mintacoin/lib/mintacoin/payments/payments.ex
@@ -0,0 +1,47 @@
+defmodule Mintacoin.Payments do
+  @moduledoc """
+  This module is responsible for doing the CRUD operations for Payments
+  """
+
+  alias Ecto.{NoResultsError, Changeset, UUID}
+  alias Ecto.Query.CastError
+  alias Mintacoin.{Repo, Payment}
+
+  @type id :: UUID.t()
+  @type code :: String.t()
+  @type changes :: map()
+  @type parameter :: keyword()
+  @type error :: Changeset.t() | :not_found
+
+  @spec create(changes :: changes()) :: {:ok, Payment.t()} | {:error, error()}
+  def create(changes) do
+    %Payment{}
+    |> Payment.create_changeset(changes)
+    |> Repo.insert()
+  end
+
+  @spec retrieve(id :: id()) :: {:ok, Payment.t()} | {:error, error()}
+  def retrieve(id) do
+    {:ok, Repo.get!(Payment, id)}
+  rescue
+    CastError -> {:error, :not_found}
+    NoResultsError -> {:error, :not_found}
+  end
+
+  @spec update(id :: id(), changes :: changes()) :: {:ok, Payment.t()} | {:error, error()}
+  def update(id, changes) do
+    id
+    |> retrieve()
+    |> persist_changes(changes)
+  end
+
+  @spec persist_changes({:ok, Payment.t()} | {:error, error()}, changes :: changes()) ::
+          {:ok, Payment.t()} | {:error, error()}
+  defp persist_changes({:ok, %Payment{} = payment}, changes) do
+    payment
+    |> Payment.changeset(changes)
+    |> Repo.update()
+  end
+
+  defp persist_changes({:error, _error} = error, _changes), do: error
+end
