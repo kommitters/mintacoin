4cd726f3b7383e91ba516e28c714a52f4efe6ab0
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-29 09:27:54 -0500

    Add Mintacoin.Crypto boundary

---
+++ apps/mintacoin/lib/mintacoin/crypto.ex
@@ -0,0 +1,22 @@
+defmodule Mintacoin.Crypto do
+  @moduledoc """
+  This module provides the boundary for the Crypto's transactions.
+  """
+
+  @behaviour Mintacoin.Crypto.Spec
+
+  @impl true
+  def create_account(params), do: impl().create_account(params)
+
+  @impl true
+  def create_asset(params), do: impl().create_asset(params)
+
+  @impl true
+  def authorize_asset(params), do: impl().authorize_asset(params)
+
+  @impl true
+  def process_payment(params), do: impl().process_payment(params)
+
+  @spec impl() :: atom()
+  defp impl, do: Application.get_env(:crypto, :impl)
+end
