fe6781c5f23591b9b049e34c24ab996713601ee8
CristhianRodriguezMolina <camilo.r13@hotmail.com>
2022-06-30 15:04:12 -0500

    Added status type to crypto spec

--- apps/mintacoin/lib/mintacoin/crypto/spec.ex
+++ apps/mintacoin/lib/mintacoin/crypto/spec.ex
@@ -3,11 +3,13 @@ defmodule Mintacoin.Crypto.Spec do
   Defines contracts for the transactions available in the Crypto layer.
   """
 
-  @callback create_account(map()) :: {:ok | :error, struct()}
+  @type status :: :ok | :error
 
-  @callback create_asset(map()) :: {:ok | :error, struct()}
+  @callback create_account(map()) :: {status(), struct()}
 
-  @callback authorize_asset(map()) :: {:ok | :error, struct()}
+  @callback create_asset(map()) :: {status(), struct()}
 
-  @callback process_payment(map()) :: {:ok | :error, struct()}
+  @callback authorize_asset(map()) :: {status(), struct()}
+
+  @callback process_payment(map()) :: {status(), struct()}
 end
