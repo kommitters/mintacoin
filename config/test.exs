cb3932e878829559468862aa68e7a5aee3dc94dc
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-06-30 12:14:59 -0500

    Add stellar_sdk dep

--- config/test.exs
+++ config/test.exs
@@ -14,6 +14,8 @@ config :mintacoin, Mintacoin.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
+config :stellar_sdk, network: :test
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :mintacoin_web, MintacoinWeb.Endpoint,
