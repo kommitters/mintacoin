34674003804757f6427960ae742a5fa001b3f627
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-07-08 09:49:42 -0500

    Improve Core.Accounts.create pipeline

--- apps/mintacoin/lib/mintacoin/core/accounts.ex
+++ apps/mintacoin/lib/mintacoin/core/accounts.ex
@@ -1,13 +1,15 @@
 defmodule Mintacoin.Core.Accounts do
   @moduledoc """
-
+  This module defines the pipeline with the business logic for Account operations
   """
 
-  alias Mintacoin.Accounts, as: AccountsDB
-
   alias Mintacoin.{
+    Repo,
+    Account,
+    Accounts,
     Blockchain,
     Blockchains,
+    BlockchainEvent,
     BlockchainEvents,
     Wallet,
     Wallets,
@@ -16,27 +18,33 @@ defmodule Mintacoin.Core.Accounts do
     Encryption
   }
 
-  alias Ecto.Multi
+  alias Ecto.{Multi, Changeset}
 
+  @starting_balance 1.5
+
+  @type error :: Changeset.t() | :blockchain_not_found
+  @type resource :: Account.t() | Wallet.t() | BlockchainEvent.t()
+
+  @spec create(params :: map()) :: {:ok, Account.t()} | {:error, error()}
   def create(params),
     do:
       Multi.new()
       |> Multi.run(:create_account_step, create_account(params))
       |> Multi.run(:create_wallet_step, create_wallet(params))
       |> Multi.run(:create_blockchain_event_step, &create_blockchain_event/2)
-      |> Mintacoin.Repo.transaction()
+      |> Repo.transaction()
       |> handle_multi_response()
 
   defp create_account(params) do
     fn _repo, _ ->
       params
-      |> AccountsDB.create()
+      |> Accounts.create()
       |> format_response()
     end
   end
 
   defp create_wallet(%{blockchain: blockchain}) do
-    fn _repo, %{create_account_step: %{id: account_id, signature: signature}} ->
+    fn _repo, %{create_account_step: %Account{id: account_id, signature: signature}} ->
       case Blockchains.retrieve_by(name: blockchain) do
         {:ok, %Blockchain{id: blockchain_id}} ->
           {address, secret} = Crypto.random_keypair()
@@ -51,6 +59,9 @@ defmodule Mintacoin.Core.Accounts do
           }
           |> Wallets.create()
           |> format_response()
+
+        {:error, :not_found} ->
+          {:error, :blockchain_not_found}
       end
     end
   end
@@ -58,29 +69,27 @@ defmodule Mintacoin.Core.Accounts do
   defp create_blockchain_event(_repo, %{
          create_wallet_step: %Wallet{address: address, blockchain_id: blockchain_id}
        }) do
+    event_payload =
+      Map.from_struct(%AccountCreated{
+        destination: address,
+        balance: @starting_balance
+      })
+
     %{
       blockchain_id: blockchain_id,
       event_type: :create_account,
-      event_payload: %AccountCreated{
-        destination: address,
-        balance: 1.5
-      }
+      event_payload: event_payload
     }
     |> BlockchainEvents.create()
     |> format_response()
   end
 
-  @spec handle_multi_response(response :: tuple()) :: tuple()
-  defp handle_multi_response({:ok, %{create_event_step: account}}),
-    do: format_response(account)
+  @spec handle_multi_response(response :: tuple()) :: {:ok, Account.t()} | {:error, error()}
+  defp handle_multi_response({:ok, %{create_account_step: account}}), do: {:ok, account}
+  defp handle_multi_response({:error, _step, error, _result}), do: {:error, error}
 
-  defp handle_multi_response({:error, :create_account_step, error, _result}), do: {:error, error}
-  defp handle_multi_response(_response), do: {:error, "Error creating the account"}
-
-  @spec format_response(response :: tuple() | map()) :: tuple()
-  defp format_response({:error, reason}) when is_binary(reason),
-    do: {:error, reason}
-
-  defp format_response({:ok, response}), do: {:ok, response}
-  defp format_response(response), do: {:ok, response}
+  @spec format_response(response :: {:ok, resource()} | {:error, error()}) ::
+          {:ok, resource()} | {:error, error()}
+  defp format_response({:ok, resource}), do: {:ok, resource}
+  defp format_response({:error, reason}), do: {:error, reason}
 end
