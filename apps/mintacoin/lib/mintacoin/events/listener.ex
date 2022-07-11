3f697638c834babd782bf5b984c9f85690778067
CristhianRodriguezMolina <camilo.r13@hotmail.com>
2022-07-11 12:01:01 -0500

    Listener tests, blockchain event factory modified

--- apps/mintacoin/lib/mintacoin/events/listener.ex
+++ apps/mintacoin/lib/mintacoin/events/listener.ex
@@ -1,6 +1,15 @@
 defmodule Mintacoin.Events.Listener do
+  @moduledoc """
+  This module is responsible for listening the events created through the BlockchainEvents table.
+
+   - The database notifier event is `event_created`
+  """
+
   use GenServer
 
+  alias Mintacoin.{Events.Structs.AccountCreated, BlockchainEvent}
+
+  @spec child_spec(opts :: Keyword.t()) :: map()
   def child_spec(opts) do
     %{
       id: Keyword.get(opts, :id, __MODULE__),
@@ -8,8 +17,10 @@ defmodule Mintacoin.Events.Listener do
     }
   end
 
-  def start_link(opts \\ []),
+  @spec start_link(opts :: Keyword.t()) :: GenServer.on_start()
+  def(start_link(opts \\ []),
     do: GenServer.start_link(__MODULE__, opts)
+  )
 
   @impl true
   def init(opts) do
@@ -22,12 +33,16 @@ defmodule Mintacoin.Events.Listener do
 
   @impl true
   def handle_info({:notification, _pid, _ref, "event_created", payload}, _state) do
-    with {:ok, data} <- Jason.decode(payload, keys: :atoms) do
-      IO.inspect(data, label: "Event Created")
+    with {:ok, data} <- Jason.decode(payload, keys: :atoms),
+         %BlockchainEvent{} = blockchain_event <- struct!(BlockchainEvent, data) do
+      payload = cast_payload(blockchain_event)
 
-      {:noreply, :event_handled}
+      {:noreply, payload}
     else
       error -> {:stop, error, []}
     end
   end
+
+  defp cast_payload(%{event_type: "create_account", event_payload: event_payload}),
+    do: struct(AccountCreated, event_payload)
 end
---
+++ apps/mintacoin/test/mintacoin/events/listener_test.exs
@@ -0,0 +1,67 @@
+defmodule Mintacoin.Events.ListenerTest do
+  @moduledoc """
+
+  """
+
+  use ExUnit.Case
+
+  import Mintacoin.Factory
+
+  alias Mintacoin.Events.{Listener, Structs.AccountCreated}
+  alias Ecto.Adapters.SQL.Sandbox
+
+  setup do
+    :ok = Sandbox.checkout(Mintacoin.Repo)
+
+    %{pid: self()}
+  end
+
+  describe "handle_info/2" do
+    test "with valid payload", %{pid: pid} do
+      %{event_payload: %{balance: balance, destination: destination}} =
+        blockchain_event =
+        :blockchain_event
+        |> params_for()
+
+      encoded_blockchain_event = Jason.encode!(blockchain_event)
+
+      {:noreply,
+       %AccountCreated{
+         balance: ^balance,
+         destination: ^destination
+       }} =
+        Listener.handle_info(
+          {:notification, pid, "ref", "event_created", encoded_blockchain_event},
+          []
+        )
+    end
+
+    test "with empty payload", %{pid: pid} do
+      blockchain_event =
+        :blockchain_event
+        |> params_for(event_payload: %{})
+        |> Jason.encode!()
+
+      {:noreply, %AccountCreated{balance: nil, destination: nil}} =
+        Listener.handle_info(
+          {:notification, pid, "ref", "event_created", blockchain_event},
+          []
+        )
+    end
+  end
+
+  test "Assert notification received in handle_info", %{pid: pid} do
+    blockchain_event =
+      :blockchain_event
+      |> params_for()
+      |> Jason.encode!()
+
+    Process.send(
+      pid,
+      {:notification, pid, "ref", "event_created", blockchain_event},
+      []
+    )
+
+    assert_receive({:notification, ^pid, "ref", "event_created", ^blockchain_event})
+  end
+end
